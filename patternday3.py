117aa4d26442940efdb55cd85a957eafb2160040
Madhuri <madhureddy162004@gmail.com>
2025-12-15 23:52:52 +0530

    pattern day3

---
+++ patternday3.py
@@ -0,0 +1,13 @@
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+rows=5
+for i in range(1,rows+1):
+    for j in range(1,rows+1):
+        if i==rows or j==rows or i+j>=rows+1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
